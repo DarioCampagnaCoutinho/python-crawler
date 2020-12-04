1e443aaa4369ad6eb264e862419e2087ba8ce0de
Dario Campagna Coutinho <campagnacoutinho67@gmail.com>
2020-12-04 19:00:33 -0300

    Requisiçao de busca e arquivo requirements.txt.

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print('Olá Python')
+import requests
+
+URL_AUTOMOVEIS = 'https://django-anuncios.solyd.com.br/automoveis/'
+
+
+def buscar(url):
+    try:
+        resposta = requests.get(url)
+        if resposta.status_code == 200:
+            print(resposta.text)
+        else:
+            print('Erro ao fazer requisição!!')
+    except Exception as error:
+        print('Erro ao fazer requisição!!')
+        print(error)
+
+
+buscar(URL_AUTOMOVEIS)
